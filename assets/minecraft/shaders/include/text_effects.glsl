c728a23544accad654e54ccde97c45843574eb61
iCodeTits <smurfsmurfy222@gmail.com>
2024-02-06 00:32:26 -0500

    fixes

--- assets/minecraft/shaders/include/text_effects.glsl
+++ assets/minecraft/shaders/include/text_effects.glsl
@@ -207,8 +207,10 @@ void apply_gradient(vec3 color1, vec3 color2) {
 }
 
 void apply_rainbow() {
-    textData.color.rgb = hsvToRgb(vec3(0.005 * (textData.position.x + textData.position.y) - GameTime * 300.0, 0.7, 1.0));
+    textData.color.rgb = vec3(textData.position.x, textData.position.y, 0.0);
     if(textData.isShadow) textData.color.rgb *= 0.25;
+    // textData.color.rgb = hsvToRgb(vec3(0.005 * (textData.position.x + textData.position.y) - GameTime * 300.0, 0.7, 1.0));
+    // if(textData.isShadow) textData.color.rgb *= 0.25;
 }
 
 void apply_colorswap(float speed, float hue, float brightness) {
@@ -490,7 +492,7 @@ bool applySpheyaPack9() {
     if(vctfx_changedScale < 0.5) {
         textData.uv = texCoord0;
     }
-    textData.position = vctfx_screenPos.xy// * uvSize * 256.0 / innerSize;
+    textData.position = vctfx_screenPos.xy * uvSize * 256.0 / innerSize;
     textData.characterPosition = 0.5 * (innerMin + innerMax) * uvSize * 256.0 / innerSize;
     if(textData.isShadow) { 
         textData.characterPosition += vec2(-1.0, 1.0);
