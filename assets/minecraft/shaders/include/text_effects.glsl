7c8a83645847f8e9682d1d098bdc785265097e55
blakebruner <blakebruner00@gmail.com>
2024-02-05 21:21:32 -0800

    fixes

--- assets/minecraft/shaders/include/text_effects.glsl
+++ assets/minecraft/shaders/include/text_effects.glsl
@@ -490,7 +490,7 @@ bool applySpheyaPack9() {
     if(vctfx_changedScale < 0.5) {
         textData.uv = texCoord0;
     }
-    textData.position = vctfx_screenPos.xy * uvSize * 256.0 / innerSize;
+    textData.position = vctfx_screenPos.xy * textData.uv * 256.0;
     textData.characterPosition = 0.5 * (innerMin + innerMax) * uvSize * 256.0 / innerSize;
     if(textData.isShadow) { 
         textData.characterPosition += vec2(-1.0, 1.0);
