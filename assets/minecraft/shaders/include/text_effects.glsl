68f637c31d4ca30d6efdc2e19165274505fdf37f
iCodeTits <smurfsmurfy222@gmail.com>
2024-02-05 23:09:32 -0500

    fixes?

--- assets/minecraft/shaders/include/text_effects.glsl
+++ assets/minecraft/shaders/include/text_effects.glsl
@@ -208,7 +208,7 @@ void apply_gradient(vec3 color1, vec3 color2) {
 
 void apply_rainbow() {
     // textData.shouldScale = true;
-    textData.color.rgb = hsvToRgb(vec3(0.005 * (textData.localPosition.x + textData.localPosition.y) - GameTime * 300.0, 0.7, 1.0));
+    textData.color.rgb = hsvToRgb(vec3(0.005 * (textData.characterPosition.x + textData.characterPosition.y) - GameTime * 300.0, 0.7, 1.0));
     if(textData.isShadow) textData.color.rgb *= 0.25;
 }
 
