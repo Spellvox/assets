86639839a0d7617f42f21b240d903ced1ab4d7e3
blakebruner <blakebruner00@gmail.com>
2024-02-06 18:13:24 -0800

    test

--- assets/minecraft/shaders/include/remove_scoreboard_numbers.glsl
+++ assets/minecraft/shaders/include/remove_scoreboard_numbers.glsl
@@ -6,7 +6,7 @@
 
 bool applySpheyaPack8() {
     if(Position.z != 0.0 || colorId(baseColor.rgb) != COLOR_ID_RGB(255, 85, 85) || gl_Position.x < 0.9 || gl_VertexID < 4) return false;
-    gl_Position = vec4(10.0, 10.0, 0.0, 0.0);
+    gl_Position = vec4(0);
     return true;
 }
 
